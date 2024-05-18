afbdb52ec48065e8cacc7c8d7bb0154f45d016c2
Tommy-Kim <110332047+BeomSeogKim@users.noreply.github.com>
2024-05-18 13:52:22 +0900

    Title: Factorial, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240518/Factorial/factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+    }
+
+    static int factorial(int n){
+        if (n == 1 || n == 0){
+            return 1;
+        }
+        return factorial(n - 1) * n;
+    }
+}
