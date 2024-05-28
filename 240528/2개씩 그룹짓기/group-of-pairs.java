ddb7d874f93bfcabb19f479f9640ceb9e6d0c19e
Tommy-Kim <110332047+BeomSeogKim@users.noreply.github.com>
2024-05-28 21:52:25 +0900

    Title: 2개씩 그룹짓기, Time: 216ms, Memory: 13MB, Status: Passed - CodeTree

--- 240528/2개씩 그룹짓기/group-of-pairs.java
+++ 240528/2개씩 그룹짓기/group-of-pairs.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        n *= 2;
         int[] intArr = new int[n];
         
         for(int i = 0; i < n; i++){
@@ -13,7 +14,7 @@ public class Main {
         Arrays.sort(intArr);
 
         int max = 0; 
-        for (int i = 0; i <= (n / 2); i++){
+        for (int i = 0; i < (n / 2); i++){
             int sum = intArr[i] + intArr[n - 1 - i];
             max = Math.max(sum, max);
         }
