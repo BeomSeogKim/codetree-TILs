9ecbd85d867ac242cb855083b2facf952d9f9c69
Tommy-Kim <110332047+BeomSeogKim@users.noreply.github.com>
2024-08-20 21:18:03 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240820/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
+++ 240820/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -26,6 +26,7 @@ public class Main {
         
         if (visited[rowSize - 1][colSize - 1]) {
             System.out.println(1);
+            return;
         }
         
         System.out.println(0);
