d1caa02a18d2b083c57aa91f2868284c9851d3f2
Tommy-Kim <110332047+BeomSeogKim@users.noreply.github.com>
2024-08-20 21:20:50 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 328ms, Memory: 20MB, Status: Passed - CodeTree

--- 240820/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
+++ 240820/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     static int[] dx = {1,0};
-    static int[] dy = {0,-1};
+    static int[] dy = {0,1};
     static int[][] grid;
     static boolean[][] visited;
 
@@ -21,7 +21,7 @@ public class Main {
                 grid[row][col] = sc.nextInt();
             }
         }
-        visited[0][0] = true;
+
         DFS(0,0);
         
         if (visited[rowSize - 1][colSize - 1]) {
@@ -33,13 +33,16 @@ public class Main {
     }
 
     static void DFS(int row, int col) {
-        
+        visited[row][col] = true;
+
         for(int d = 0; d < 2; d++) {
             int nRow = row + dx[d];
             int nCol = col + dy[d];
-            if(nRow >= 0 && nRow < visited.length && nCol >= 0 && nCol < visited[0].length && !visited[nRow][nCol] && grid[nRow][nCol] == 1) {
-                visited[nRow][nCol] = true;
-                DFS(nRow,nCol);
+            if(nRow >= 0 && nRow < visited.length && nCol >= 0 && nCol < visited[0].length) {
+                if (!visited[nRow][nCol] && grid[nRow][nCol] == 1) {
+                    DFS(nRow,nCol);
+                }
+            
             }
         }
     }
