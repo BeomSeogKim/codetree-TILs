d3cffa3b04db7aeb329a1f1baf60302091bc83b5
Tommy-Kim <110332047+BeomSeogKim@users.noreply.github.com>
2024-08-26 23:07:04 +0900

    Title: 마을 구분하기, Time: 175ms, Memory: 12MB, Status: Passed - CodeTree

--- 240826/마을 구분하기/seperate-village.java
+++ 240826/마을 구분하기/seperate-village.java
@@ -1,65 +1,82 @@
 import java.util.Scanner;
-import java.util.Objects;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
-
 public class Main {
+    public static final int DIR_NUM = 4;
+    public static final int MAX_NUM = 25;
     
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-
-    static int[][] grid;
-    static boolean[][] visited;
+    public static int n;
+    public static int[][] grid = new int[MAX_NUM][MAX_NUM];
+    public static boolean[][] visited = new boolean[MAX_NUM][MAX_NUM];
+    public static ArrayList<Integer> peopleNums = new ArrayList<>();
+    public static int peopleNum;
+    
+    // 탐색하는 위치가 격자 범위 내에 있는지 여부를 반환합니다.
+    public static boolean inRange(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < n;
+    }
+    
+    // 탐색하는 위치로 움직일 수 있는지 여부를 반환합니다.
+    public static boolean canGo(int x, int y) {
+        if(!inRange(x, y))
+            return false;
+    
+        if(visited[x][y] || grid[x][y] == 0)
+            return false;
+    
+        return true;
+    }
+    
+    public static void DFS(int x, int y) {
+        //0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3: 위쪽
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+    
+        // 네 방향에 각각에 대하여 DFS 탐색을 합니다.
+        for(int dir = 0; dir < DIR_NUM; dir++) {
+            int newX = x + dx[dir];
+            int newY = y + dy[dir];
+    
+            if(canGo(newX, newY)){
+                visited[newX][newY] = true;
+                // 마을에 존재하는 사람을 한 명 추가해줍니다.
+                peopleNum++;
+                DFS(newX, newY);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        List<Integer> peoples = new ArrayList<>();
-        
-        // initialize 
-        grid = new int[size][size];
-        visited = new boolean[size][size];
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
 
-        for (int row = 0; row < size; row++){
-            for(int col = 0; col < size; col++){
-                grid[row][col] = sc.nextInt();
-            }
-        }
+        // 격자의 각 위치에서 탐색을 시작할 수 있는 경우
+        // 한 마을에 대한 DFS 탐색을 수행합니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(canGo(i, j)) {
+                    // 해당 위치를 방문할 수 있는 경우 visited 배열을 갱신하고
+                    // 새로운 마을을 탐색한다는 의미로 peopleNum을 1으로 갱신합니다.
+                    visited[i][j] = true;
+                    peopleNum = 1;
 
-        for(int row = 0; row < size; row++) {
-            for(int col = 0; col < size; col++) {
-                if (grid[row][col] == 1 && !visited[row][col]) {
-                    visited[row][col] = true;
-                    int neighbor = DFS(row, col);
-                    peoples.add(neighbor);
+                    DFS(i, j);
+
+                    // 한 마을에 대한 탐색이 끝난 경우 마을 내의 사람 수를 저장합니다.
+                    peopleNums.add(peopleNum);
                 }
             }
         }
 
-        Collections.sort(peoples);
-        
-        System.out.println(peoples.size());
-        for(Integer neighbor : peoples){
-            System.out.println(neighbor);
-        }
+        // 각 마을 내 사람의 수를 오름차순으로 정렬합니다.
+        Collections.sort(peopleNums);
 
-    }
-
-    static int DFS(int row, int col){
-        visited[row][col] = true;
-        
-        for(int d = 0; d < 4; d++){
-            int newRow = row + dx[d];
-            int newCol = col + dy[d];
-            int size = grid.length;
-            if (newRow >= 0 && newRow < size && newCol >= 0 && newCol < size ) {
-                if (grid[newRow][newCol] == 1 && !visited[newRow][newCol]) {
-                    return DFS(newRow, newCol) + 1;
-                }
-            }
-        }
-        return 1;
+        System.out.println((int) peopleNums.size());
+        for(int i = 0; i < (int) peopleNums.size(); i++)
+            System.out.println(peopleNums.get(i));
     }
 }
